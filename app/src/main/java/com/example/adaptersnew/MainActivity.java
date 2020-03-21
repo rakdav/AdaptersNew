073e42ebf2bbcd8bff5c1d98558e018e9d10ec1c
Andrey Dubinin <rakdav@inbox.ru>
2020-03-21 17:07:53 +0200

    first

--- app/src/main/java/com/example/adaptersnew/MainActivity.java
+++ app/src/main/java/com/example/adaptersnew/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.adaptersnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,17 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView countiesList=findViewById(R.id.countriesList);
+        final ListView countiesList=findViewById(R.id.countriesList);
         selection=findViewById(R.id.selection);
         countries=getResources().getStringArray(R.array.countries);
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,countries);
+//        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,countries);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_multiple_choice,countries);
         countiesList.setAdapter(adapter);
         countiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String selectedItem=countries[i];
-                selection.setText(selectedItem);
+//                String selectedItem=countries[i];
+//                selection.setText(selectedItem);
+                SparseBooleanArray sp=countiesList.getCheckedItemPositions();
+                String selectedItems="";
+                for(int c=0;i<countries.length;c++)
+                {
+                    if(sp.get(c)) selectedItems+=countries[c]+",";
+                }
+                selection.setText(selectedItems);
             }
         });
 
